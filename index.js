6dc7dbaf431bad86751bd0c801690b2e6469e1e0
Queenvictoria <snow@hollysydney.com>
2018-10-30 13:29:29 +1100

    Framing shim.

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-videojs-vr-shim'
+  name: 'ember-videojs-vr-shim',
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    const baseDirectory = 'node_modules/videojs-vr/dist';
+
+    app.import(path.join(videojsDirectory, 'video-js.css'));
+
+    app.import({
+      development: path.join(baseDirectory, 'video-vr.js'),
+      production: path.join(baseDirectory, 'videojs-vr.min.js'),
+    });
+    app.import('vendor/shims/videojs-vr.js');
+  },
 };
